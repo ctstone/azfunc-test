fee30be396c29d978f088e63d35952483f52decd
Chris Stone <chstone@microsoft.com>
2017-05-10 15:39:11 -0400

    fix bindings

--- request/index.js
+++ request/index.js
@@ -1,15 +1,16 @@
 const request = require('request');
 
-module.exports = (context, req, res) => {
-  const url = req.url || 'http://www.bing.com/';
+module.exports = (context) => {
+  const url = context.req.url || 'http://www.bing.com/';
   context.log.info(`Requesting ${url}`);
+  context.log.info(arguments);
  
   request.get(url, (err, resp, body) => {
     if (err) {
       return context.done(err);
     }
     context.log.info(`Got HTTP ${resp.statusCode}`)
-    res.body = { statusCode: resp.statusCode, length: body.length, headers: resp.headers };
+    context.res.body = { statusCode: resp.statusCode, length: body.length, headers: resp.headers };
     context.done();
   });
 }
